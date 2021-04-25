5b7d649a26b82ec81269bd1ee893a2810a0bb664
neeraj15022001 <gneeraj32595@gmail.com>
2021-04-26 01:55:27 +0530

    added files

---
+++ main.js
@@ -0,0 +1,9 @@
+const owner = document.getElementById("owner-field")
+const car = document.getElementById("car-field")
+const license = document.getElementById("license-field")
+const entryDate = document.getElementById("entry-date-field")
+const endDate = document.getElementById("end-date-field")
+const addCarButton = document.getElementById("add-car-button")
+addCarButton.addEventListener("click", () => {
+    
+})
