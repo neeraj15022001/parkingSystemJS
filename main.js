57f400e182c65a9313da548d508d8043f9dd24a3
neeraj15022001 <gneeraj32595@gmail.com>
2021-04-26 19:35:35 +0530

    added files

--- main.js
+++ main.js
@@ -1,9 +1,56 @@
-const owner = document.getElementById("owner-field")
-const car = document.getElementById("car-field")
-const license = document.getElementById("license-field")
-const entryDate = document.getElementById("entry-date-field")
-const endDate = document.getElementById("end-date-field")
-const addCarButton = document.getElementById("add-car-button")
-addCarButton.addEventListener("click", () => {
-    
-})
+const addCarButton = document.getElementById("add-car-button");
+const carEntry = document.getElementById("car-entries");
+const details = document.getElementById("details-form");
+const searchField = document.getElementById("search-field");
+addCarButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const owner = document.getElementById("owner-field").value;
+  const car = document.getElementById("car-field").value;
+  const license = document.getElementById("license-field").value;
+  const entryDate = document.getElementById("entry-date-field").value;
+  const endDate = document.getElementById("end-date-field").value;
+  insertEntry({
+    owner: owner,
+    car: car,
+    license: license,
+    entryDate: entryDate,
+    endDate: endDate,
+  });
+  details.reset();
+});
+
+function insertEntry({ owner, car, license, entryDate, endDate }) {
+  const elementToBeAppended = `
+    <tr>
+        <td class="owner-name">${owner}</td>
+        <td>${car}</td>
+        <td>${license}</td>
+        <td>${entryDate}</td>
+        <td>${endDate}</td>
+        <td><button class="btn btn-danger" onclick="removeCell(event)">Remove</button></td>
+    </tr>
+    `;
+  carEntry.innerHTML += elementToBeAppended;
+}
+
+function removeCell(e) {
+  e.target.parentElement.parentElement.remove();
+}
+
+searchField.addEventListener("keyup", (e) => {
+  searchElement({ input: e.target.value });
+});
+
+function searchElement({ input }) {
+  let txtValue;
+  const query = input.toUpperCase();
+  owners = document.querySelectorAll(".owner-name");
+  for (i = 0; i < owners.length; i++) {
+    txtValue = owners[i].textContent || owners[i].innerText;
+    if (txtValue.toUpperCase().indexOf(query) > -1) {
+      owners[i].parentElement.style.display = "";
+    } else {
+      owners[i].parentElement.style.display = "none";
+    }
+  }
+}
